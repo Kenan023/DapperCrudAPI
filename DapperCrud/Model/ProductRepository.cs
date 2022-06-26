e83af25f7cc43c9b11e0466811bb3d2670b43180
Kenan023 <kenanhesenov2322@mail.ru>
2022-06-26 16:16:48 +0400

    Dapper Crud API

---
+++ DapperCrud/Model/ProductRepository.cs
@@ -0,0 +1,72 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperCrud.Model
+{
+    public class ProductRepository
+    {
+        private string connectionString;
+
+        public ProductRepository()
+        {
+            connectionString = "Server=DESKTOP-EIQOHNN\\SQLEXPRESS;Database=DAPPERDB;Trusted_Connection=True;MultipleActiveResultSets=true";
+        }
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(connectionString);
+            }
+        }
+        public void Add(Product prod)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"Insert INTO Products(Name,Quantity,Price) VALUES(@Name,@Quantity,@Price)";
+                dbConnection.Open();
+                dbConnection.Execute(sQuery, prod);
+            }
+        }
+        public IEnumerable<Product> GetAll()
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"Select *from Products";
+                dbConnection.Open();
+                return dbConnection.Query<Product>(sQuery);
+            }
+        }
+        public Product GetById(int id)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"Select *from Products Where ProductId=@Id";
+                dbConnection.Open();
+                return dbConnection.Query<Product>(sQuery, new { Id = id }).FirstOrDefault();
+            }
+        }
+        public void Delete(int id)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"Delete From Products Where ProductId=@Id";
+                dbConnection.Open();
+                dbConnection.Execute(sQuery, new { Id = id });
+            }
+        }
+        public void Update(Product prod)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                string sQuery = @"Update Products Set Name=@Name,Quantity=@Quantity,Price=@Price Where ProductId=@ProductId";
+                dbConnection.Open();
+                dbConnection.Query(sQuery, prod);
+            }
+        }
+    }
+}
